8803c1d8323472e11a09ece7b449944cff878ddb
mohammadlebzo <mohlebzo1@gmail.com>
2022-05-09 15:24:50 +0300

    finished task 14

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
-DROP TABLE IF EXISTS movies;
 
-CREATE TABLE IF NOT EXISTS movies (
-    id varchar(255),
+
+CREATE TABLE movies (
+    id int,
     title varchar(255),
     release_date varchar(255),
     poster_path varchar(255),
